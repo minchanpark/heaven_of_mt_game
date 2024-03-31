796951683c61f9ad3bd32b2f214578fd19739e24
GwangjinJeong1 <dbsusqn12@gmail.com>
2024-04-01 01:15:42 +0900

    기차게임 로직 완성 및 타이머 적용

--- lib/game_page/train/train_game_web.dart
+++ lib/game_page/train/train_game_web.dart
@@ -26,9 +26,12 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
   final CardSwiperController controller = CardSwiperController();
   List<GameCard> cards = []; // cards 변수를 초기화
   List<GameContents> randomTrain = [];
-  bool _isCounting = false;
-  bool _isShowing = false;
-  bool _showFinal = false;
+  bool _isCounting = false; // 타이머 진행 여부
+  bool _isShowing = false; // 문제 보여짐 여부
+  bool _showFinal = false; // 문제 타이머 종료 여부
+  bool _isAnswered = false;
+  late Timer countdownTimer;
+  late Timer problemTimer;
   int _count = 3;
   @override
   void initState() {
@@ -49,40 +52,63 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
     super.dispose();
   }
 
+  // 문제 보여주기 전 3초 타이머 함수
   void _startCountdown() {
     setState(() {
-     _isCounting = true;
-     _count = 3;
+      _isCounting = true;
+      _count = 3;
     });
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      if(_count > 1) {
+    debugPrint(
+        "_isCounting: $_isCounting, _isShowing: $_isShowing, _showFinal: $_showFinal");
+    countdownTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_count > 1) {
         setState(() {
           _count--;
         });
       } else {
         timer.cancel();
         setState(() {
-         _isCounting = false; 
-         _isShowing = true;
-         _count = 10;
+          _isCounting = false;
+          _isShowing = true;
+          _count = 10;
         });
+        debugPrint(
+            "_isCounting: $_isCounting, _isShowing: $_isShowing, _showFinal: $_showFinal");
         _showProblem();
       }
     });
   }
+
+  // 문제가 보여진 후 10초 타이머 함수
   void _showProblem() {
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      if(_count > 1) {
+    problemTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_count > 1) {
         setState(() {
           _count--;
         });
+        debugPrint(
+            "_isCounting: $_isCounting, _isShowing: $_isShowing, _showFinal: $_showFinal");
       } else {
         timer.cancel();
         setState(() {
           _isShowing = false;
           _showFinal = true;
         });
+        debugPrint(
+            "_isCounting: $_isCounting, _isShowing: $_isShowing, _showFinal: $_showFinal");
       }
+    });
+  }
+
+  // 타이머 초기화
+  void _resetTimer() {
+    setState(() {
+      countdownTimer.cancel();
+      problemTimer.cancel();
+      _isCounting = false;
+      _isShowing = false;
+      _showFinal = false;
+      _count = 3;
     });
   }
 
@@ -119,7 +145,10 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
                   focusNode: focusNode,
                   onKey: (RawKeyEvent event) {
                     if (event is RawKeyDownEvent) {
-                      if (event.logicalKey == LogicalKeyboardKey.escape) {
+                      if (_isCounting || _isShowing) {
+                        return;
+                      } else if (event.logicalKey ==
+                          LogicalKeyboardKey.escape) {
                         Navigator.of(context).pop();
                       } else if (event.logicalKey ==
                           LogicalKeyboardKey.arrowLeft) {
@@ -157,6 +186,10 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
                         children: [
                           IconButton(
                             onPressed: () {
+                              // 타이머 진행중이면 버튼 disable
+                              if (_isCounting || _isShowing) {
+                                return;
+                              }
                               Navigator.of(context).pop();
                             },
                             color: Colors.white,
@@ -169,13 +202,23 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
                             '${currentCardIndex + 1}/${cards.length}',
                             style: TextStyle(
                               fontFamily: 'DungGeunMo',
-                              color: const Color.fromARGB(255, 88, 71, 71),
+                              color: Colors.white,
                               fontWeight: FontWeight.w400,
                               fontSize: width * 0.033,
                             ),
                           ),
                           const Spacer(),
-                          SizedBox(width: width * 0.039),
+                          _isShowing
+                              ? SizedBox(
+                                  width: width * 0.039,
+                                  child: Text("$_count",
+                                      style: const TextStyle(
+                                        fontFamily: 'DungGeunMo',
+                                        fontSize: 50,
+                                        color: Colors.white,
+                                      )),
+                                )
+                              : SizedBox(width: width * 0.039)
                         ],
                       ),
                       Expanded(
@@ -195,7 +238,10 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
                                 : GestureDetector(
                                     onTap: () {
                                       controller.undo();
-                                      if (currentCardIndex == 0) {
+                                      // 타이머 진행중이면 버튼 disable
+                                      if (_isCounting || _isShowing) {
+                                        return;
+                                      } else if (currentCardIndex == 0) {
                                         setState(() {
                                           isUndoButtonVisible = true;
                                         });
@@ -221,12 +267,43 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
                                 ) {
                                   currentCardIndex = index;
                                   return _isCounting
-                                  ? Text("$_count", style: const TextStyle(fontSize: 70))
-                                  : _isShowing // 시작 버튼;
-                                    ? cards[index]
-                                    : _showFinal
-                                      ? const Text("문장은 무엇일까요?", style: TextStyle(fontSize: 70))
-                                      : GestureDetector(onTap: _startCountdown, child: const Text("시작", style: TextStyle(fontSize: 70)));
+                                      // 타이머 진행 중 초 표시
+                                      ? Center(
+                                          child: Text("$_count",
+                                              style: const TextStyle(
+                                                fontFamily: 'DungGeunMo',
+                                                fontSize: 80,
+                                                color: Colors.white,
+                                              )),
+                                        )
+                                      : _isShowing
+                                          // 문제 표시
+                                          ? cards[index]
+                                          : _showFinal
+                                              // 문제 표시 후 10초가 흘렀을 때
+                                              ? _isAnswered
+                                                  ? cards[index]
+                                                  : const Center(
+                                                      child: Text("문장은 무엇일까요?",
+                                                          style: TextStyle(
+                                                            fontFamily:
+                                                                'DungGeunMo',
+                                                            fontSize: 80,
+                                                            color: Colors.white,
+                                                          )),
+                                                    )
+                                              // 타이머 시작 전
+                                              : Center(
+                                                  child: GestureDetector(
+                                                      onTap: _startCountdown,
+                                                      child: const Text("시작",
+                                                          style: TextStyle(
+                                                            fontFamily:
+                                                                'DungGeunMo',
+                                                            fontSize: 80,
+                                                            color: Colors.white,
+                                                          ))),
+                                                );
                                 },
                                 isDisabled: true,
                                 onSwipe: _onSwipe,
@@ -235,7 +312,10 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
                             ),
                             IconButton(
                               onPressed: () {
-                                if (currentCardIndex == 2) {
+                                // 타이머 진행중이면 버튼 disable
+                                if (_isCounting || _isShowing) {
+                                  return;
+                                } else if (currentCardIndex == 2) {
                                   Navigator.of(context).push(
                                     MaterialPageRoute(
                                       builder: (context) => const GameOver(
@@ -262,6 +342,36 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
                           ],
                         ),
                       ),
+                      _showFinal
+                          ? SizedBox(
+                              width: width * 0.173,
+                              height: height * 0.085,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _isAnswered = !_isAnswered;
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: _isAnswered
+                                      ? Colors.white
+                                      : const Color(0xffFF62D3),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                ),
+                                child: Text(
+                                  _isAnswered ? '돌아가기' : '문제보기',
+                                  style: const TextStyle(
+                                    fontFamily: 'DungGeunMo',
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 50,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            )
+                          : SizedBox(
+                              width: width * 0.173, height: height * 0.085),
                       SizedBox(height: height * 0.1)
                     ],
                   ),
@@ -274,6 +384,52 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
     );
   }
 
+  Widget countOrButton() {
+    if (_isShowing && !_showFinal) {
+      // 카운트 숫자 표시
+      return Visibility(
+          visible: _isShowing,
+          child: Text("$_count",
+              style: const TextStyle(
+                fontFamily: 'DungGeunMo',
+                fontSize: 80,
+                color: Colors.white,
+              )));
+    } else if (!_isShowing && _showFinal) {
+      // 문제보기 버튼
+      return Visibility(
+        visible: _showFinal,
+        child: SizedBox(
+          width: 300,
+          height: 85,
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _isAnswered = !_isAnswered;
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor:
+                  _isAnswered ? Colors.white : const Color(0xffFF62D3),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+            ),
+            child: Text(
+              _isAnswered ? '돌아가기' : '문제보기',
+              style: const TextStyle(
+                fontFamily: 'DungGeunMo',
+                fontWeight: FontWeight.w400,
+                fontSize: 50,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+    return const SizedBox();
+  }
+
   bool _onSwipe(
     int previousIndex,
     int? currentIndex,
@@ -281,6 +437,7 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
   ) {
     setState(() {
       currentCardIndex = currentIndex ?? 0; // currentIndex가 null인 경우 기본값 0으로 설정
+      _resetTimer();
     });
 
     return true;
@@ -293,16 +450,8 @@ class _TrainWebGamePageState extends State<TrainWebGame> {
   ) {
     setState(() {
       currentCardIndex = currentIndex; // currentIndex가 null인 경우 기본값 0으로 설정
+      _resetTimer();
     });
     return true;
-  }
-}
-
-class StartButton extends StatelessWidget {
-  const StartButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
